4e2c34d88821f2493094470be088476bf9311b03
Andres <andres.jaag@hotmail.es>
2022-05-24 18:31:58 -0500

    hasta enviar datos a excel sin formato

--- ConsoleAppLleida/ConsoleAppLleida/Program.cs
+++ ConsoleAppLleida/ConsoleAppLleida/Program.cs
@@ -10,15 +10,17 @@ namespace ConsoleAppLleida // Note: actual namespace depends on the project name
     
     internal class Program
     {
-        internal static int contador=0;
+        internal static int contador=0,marce=0;
         internal static SLDocument osLDocument = new SLDocument();
         internal static System.Data.DataTable dt = new System.Data.DataTable();
+        internal static string mail_id, mail_date, mail_type, file_doc_model, file_uid, unidades_certificadas, mail_from, mail_to,direccion_CC, gstatus, gstatus_aux, mail_subj, add_id, add_displaydate;
+
 
         static void Main(string[] args)
         {
             //documento excel
             
-            contador++;
+            
             //columnas
             dt.Columns.Add("Id", typeof(string));
             dt.Columns.Add("Fecha", typeof(string));
@@ -39,22 +41,33 @@ namespace ConsoleAppLleida // Note: actual namespace depends on the project name
 
 
             string path=Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\prueba.xml");
-            //UsingXmlReader("https://tsa.lleida.net/cgi-bin/mailcertapi.cgi?action=list_pdf&user=sodigsa@ec&password=TIiANcmymJ&mail_date_min=20220524213336");
-            //string pathFile = AppDomain.CurrentDomain.BaseDirectory + "miExcel.xlsx";
+            UsingXmlReader("https://tsa.lleida.net/cgi-bin/mailcertapi.cgi?action=list_pdf&user=sodigsa@ec&password=TIiANcmymJ&mail_date_min=20220524232953");
             string pathFile = AppDomain.CurrentDomain.BaseDirectory + "miExcel.xlsx";
-            irExcel(pathFile);
+            //string pathFile = AppDomain.CurrentDomain.BaseDirectory + "miExcel.xlsx";
+            //irExcel(pathFile);
            
         }
 
         private static void UsingXmlReader(string path)
         {
-            contador++;
-            string mail_id,mail_date, mail_type, file_doc_model, file_uid, mail_from, mail_to, gstatus, gstatus_aux, mail_subj, add_id;
+            
             XmlReader xmlReader=XmlReader.Create(path);
 
             while (xmlReader.Read())
             {
-                if((xmlReader.NodeType == XmlNodeType.Element)&&(xmlReader.Name== "mail_id"))
+
+                if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "mail_id"))
+                {
+                    contador++;
+                }
+                if(contador == 2)
+                {
+                    dt.Rows.Add(mail_id, mail_date, mail_type, file_doc_model, file_uid, unidades_certificadas, mail_from, mail_to, direccion_CC, gstatus, gstatus_aux, mail_subj, add_id, add_displaydate);
+                    Console.WriteLine("envio datos");
+                    contador=contador-1;
+                }
+
+                    if ((xmlReader.NodeType == XmlNodeType.Element)&&(xmlReader.Name== "mail_id"))
                 {
                    
                      mail_id = xmlReader.ReadElementContentAsString();
@@ -82,6 +95,8 @@ namespace ConsoleAppLleida // Note: actual namespace depends on the project name
                 {
                     file_uid = xmlReader.ReadElementContentAsString();
                     Console.WriteLine("file_uid= " + file_uid);
+                    unidades_certificadas = "1.00";
+                    direccion_CC = "correo@certificado.lleida.net";
 
 
                 }
@@ -125,9 +140,17 @@ namespace ConsoleAppLleida // Note: actual namespace depends on the project name
                 {
                     add_id = xmlReader.ReadElementContentAsString();
                     Console.WriteLine("add_id= " + add_id);
+                    
+
+                }
+                else if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "add_displaydate"))
+                {
+                    add_id = xmlReader.ReadElementContentAsString();
+                    Console.WriteLine("add_displaydate= " + add_id);
                     Console.WriteLine("\n");
 
                 }
+
 
             }
             
@@ -139,9 +162,9 @@ namespace ConsoleAppLleida // Note: actual namespace depends on the project name
             
 
             //registros 
-            dt.Rows.Add("pepe",19,"hombre");
+           /* dt.Rows.Add("pepe",19,"hombre");
             dt.Rows.Add("andres", 27, "hombre");
-            dt.Rows.Add("Eve", 10, "mujer");
+            dt.Rows.Add("Eve", 10, "mujer");*/
 
             //donde iniciamos
             osLDocument.ImportDataTable(1,1,dt,true);
